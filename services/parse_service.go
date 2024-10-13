9144281b935cfeeedc9092eecf6a58c19abffca9
cu8code <git8ankanroy@gmail.com>
2024-10-13 09:23:11 +0530

    added support for upload to cloudinary

--- services/parse_service.go
+++ services/parse_service.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"path"
 	"regexp"
+	"strings"
+
+	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/cloudinary/cloudinary-go/v2/api/admin"
+	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
 )
 
 func main() {
@@ -13,7 +22,6 @@ func main() {
 		fmt.Println("Error sending request:", err)
 		return
 	}
-
 	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
 	req.Header.Set("Accept-Language", "en-GB,en-US;q=0.9,en;q=0.8")
 	req.Header.Set("Cache-Control", "max-age=0")
@@ -51,12 +59,60 @@ func main() {
 		}
 	}
 
-	if len(links) == 0 {
-		fmt.Println("No links found")
+	// upload the downloaded file to cloudinary
+	for _, link := range links {
+		uploadFileToCloudinary("https://mf.nipponindiaim.com/" + link)
+	}
+}
+
+func uploadFileToCloudinary(fileURL string) {
+	// Initialize Cloudinary
+	cloudinaryURL := os.Getenv("CLOUDINARY_URL")
+	cld, err := cloudinary.NewFromURL(cloudinaryURL)
+	if err != nil {
+		fmt.Println("Error creating Cloudinary instance:", err)
 		return
 	}
 
-	for _, link := range links {
-		fmt.Println(link)
+	// Extract the file name from the file URL
+	publicID := getFileNameFromURL(fileURL)
+
+	// Check if the file already exists
+	exists, err := checkIfFileExists(cld, publicID)
+	if err != nil {
+		fmt.Println("Error checking file existence:", err)
+		return
 	}
+
+	if exists {
+		fmt.Printf("File already exists on Cloudinary: %s\n", publicID)
+		return
+	}
+
+	// Upload the file if it doesn't exist
+	resp, err := cld.Upload.Upload(context.Background(), fileURL, uploader.UploadParams{
+		PublicID: publicID,
+	})
+	if err != nil {
+		fmt.Println("Error uploading to Cloudinary:", err)
+		return
+	}
+	fmt.Printf("File uploaded successfully: %s\n", resp.SecureURL)
+}
+
+func checkIfFileExists(cld *cloudinary.Cloudinary, publicID string) (bool, error) {
+	e, _ := cld.Admin.Asset(context.Background(), admin.AssetParams{
+		PublicID: publicID,
+	})
+	log.Println(e.Error.Message)
+	if strings.Contains(e.Error.Message, "not found") {
+		return false, nil
+	}
+	return true, nil
+}
+
+func getFileNameFromURL(fileURL string) string {
+	fileName := path.Base(fileURL)
+	fileName = strings.TrimSuffix(fileName, path.Ext(fileName))
+	return fileName
 }
