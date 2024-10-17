d77b6601188eb02a22736a5718799e89e1d28de0
cu8code <git8ankanroy@gmail.com>
2024-10-17 19:37:20 +0530

    added checks & added a setup step also removed checkFileExistence as I could not get it working

--- services/parse_service.go
+++ services/parse_service.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -9,7 +10,6 @@ import (
 	"path"
 	"regexp"
 	"strings"
-	"time"
 
 	"github.com/cloudinary/cloudinary-go/v2"
 	"github.com/cloudinary/cloudinary-go/v2/api/admin"
@@ -17,43 +17,68 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
-func main() {
-	scheduler := cron.New()
-
-	// cron dose not support the L flag, for last day of the month!
-	// For April, June, September, November
-	_, err := scheduler.AddFunc("0 0 30 4,6,9,11 *", executeMonthlyTask)
-	// For January, March, May, July, August, October, December
-	_, err2 := scheduler.AddFunc("0 0 31 1,3,5,7,8,10,12 *", executeMonthlyTask)
-	// For February
-	_, err3 := scheduler.AddFunc("0 0 28 2 *", executeMonthlyTask)
-	if err != nil {
-		log.Fatal("Error scheduling task:", err)
+func assert(b bool, mess string){
+	red   := "\033[31m"
+	green := "\033[32m"
+	reset := "\033[0m"
+	if b {
+		panic(red + "Assert FAILED: " + mess + reset)
 	}
-	if err2 != nil {
-		log.Fatal("Error scheduling task:", err)
+	if os.Getenv("DEBUG") == "true" {
+		fmt.Println(green + "Assert PASSED: ", mess + reset)
 	}
-	if err3 != nil {
-		log.Fatal("Error scheduling task:", err)
-	}
-
-	scheduler.Start()
-
-	log.Println("Scheduler started. Waiting for month-end...")
-
-	select {}
 }
 
-func executeMonthlyTask() {
-	now := time.Now()
-	tomorrow := now.Add(24 * time.Hour)
-
-	if now.Month() != tomorrow.Month() {
-		log.Println("Month-end detected. Executing upload task...")
-		performUploadTask()
-	} else {
-		log.Println("Not month-end. Skipping upload task.")
+func setupCheck() {
+	if (len(os.Getenv("CLOUDINARY_URL")) < 5 ) {
+		panic("Please provied a CLOUDINARY_URL. Run `export CLOUDINARY_URL=your@url` before in your shell for linux and MacOS")
 	}
+	if os.Getenv("DEBUG") != "true" {
+		log.Printf("IGNORE THIS ONLY FOR DEV: To run the script in debug mode use `export DEBUG=\"true\"`")
+	}
+}
+
+func main() {
+	setupCheck()
+	scheduler := cron.New()
+	debug := os.Getenv("DEBUG") == "true"
+
+
+	if !debug {
+		// cron dose not support the L flag, for last day of the month!
+		// For April, June, September, November
+		_, err := scheduler.AddFunc("0 0 30 4,6,9,11 *", performUploadTask)
+		// For January, March, May, July, August, October, December
+		_, err2 := scheduler.AddFunc("0 0 31 1,3,5,7,8,10,12 *", performUploadTask)
+		// For February
+		_, err3 := scheduler.AddFunc("0 0 28 2 *", performUploadTask)
+		if err != nil {
+			log.Fatal("Error scheduling task:", err)
+		}
+		if err2 != nil {
+			log.Fatal("Error scheduling task:", err)
+		}
+		if err3 != nil {
+			log.Fatal("Error scheduling task:", err)
+		}
+		scheduler.Start()
+	} else {
+		fmt.Println("Skipping the regular scheduler as debug mode is enabled.")
+		fmt.Println("Creating a scheduler that will run every 1 minute.")
+		jobID, err := scheduler.AddFunc("* * * * *", performUploadTask)
+
+		// Need this here for proper Next time calculation
+		scheduler.Start()
+		if err != nil {
+		    fmt.Println("An error occurred: the scheduler could not be added.")
+		} else {
+			fmt.Println("Next run time for Debug Scheduler:", scheduler.Entry(jobID).Next)
+		}
+	}
+
+	log.Println("Scheduler started")
+
+	select {}
 }
 
 func performUploadTask() {
@@ -105,8 +130,12 @@ func setRequestHeaders(req *http.Request) {
 }
 
 func extractPortfolioLinks(htmlContent string) []string {
-	re := regexp.MustCompile(`Monthly portfolio for the month end.*?]+href="([^"]+)"`)
+	assert(len(htmlContent) == 0, "extractPortfolioLinks len(htmlContent) == 0")
+
+	re := regexp.MustCompile(`Monthly portfolio for the month end.*?<a[^>]+href="([^"]+)"`)
 	matches := re.FindAllStringSubmatch(htmlContent, -1)
+
+	assert(len(matches) == 0, "extractPortfolioLinks len(matches) == 0")
 
 	var links []string
 	for _, match := range matches {
@@ -114,10 +143,14 @@ func extractPortfolioLinks(htmlContent string) []string {
 			links = append(links, match[1])
 		}
 	}
+
+	assert(len(links) == 0, "extractPortfolioLinks len(links) == 0")
 	return links
 }
 
 func uploadToCloudinary(fileURL string) {
+	assert(len(fileURL) == 0, "uploadToCloudinary len(fileURL) == 0")
+
 	cld, err := cloudinary.NewFromURL(os.Getenv("CLOUDINARY_URL"))
 	if err != nil {
 		log.Println("Error creating Cloudinary instance:", err)
@@ -125,17 +158,6 @@ func uploadToCloudinary(fileURL string) {
 	}
 
 	publicID := extractFileName(fileURL)
-
-	exists, err := checkFileExistence(cld, publicID)
-	if err != nil {
-		log.Println("Error checking file existence:", err)
-		return
-	}
-
-	if exists {
-		log.Printf("File already exists on Cloudinary: %s\n", publicID)
-		return
-	}
 
 	resp, err := cld.Upload.Upload(context.Background(), fileURL, uploader.UploadParams{
 		PublicID: publicID,
@@ -149,6 +171,8 @@ func uploadToCloudinary(fileURL string) {
 }
 
 func checkFileExistence(cld *cloudinary.Cloudinary, publicID string) (bool, error) {
+	assert(cld == nil, "checkFileExistence cld == null")
+	assert(publicID == "", "checkFileExistence publicId == \"\"")
 	_, err := cld.Admin.Asset(context.Background(), admin.AssetParams{
 		PublicID: publicID,
 	})
@@ -156,6 +180,8 @@ func checkFileExistence(cld *cloudinary.Cloudinary, publicID string) (bool, erro
 }
 
 func extractFileName(fileURL string) string {
+	assert(fileURL == "", "extractFileName fileURL == \"\"")
+
 	fileName := path.Base(fileURL)
 	return strings.TrimSuffix(fileName, path.Ext(fileName))
 }
